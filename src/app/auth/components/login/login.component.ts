2f80a8c5b07ff6a1325c7dd807dc5d68e129e9db
Juan Vidal <juanmvg2003@gmail.com>
2024-04-23 23:04:53 -0500

    fue terminado el login page

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   
+  
+  constructor(
+    private formBuilder:FormBuilder,
+    private router:Router
+  ){
+    this.buildLogInForm();
+  }
+  
+  // esquema formulario
+  loginForm!:FormGroup;
+  private buildLogInForm(){
+    this.loginForm = this.formBuilder.group({
+      user: ['',
+      [Validators.required,
+      Validators.maxLength(20)]],
+
+      password: ['',
+      [Validators.required,
+      Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+    })
+  }
+
+  // validaciones
+  get userField(){ return this.loginForm.get('user'); }
+  get passwordField(){ return this.loginForm.get('password'); }
+
+  // accion del formulario
+  handleSubmit(){
+    this.router.navigate(['/']);
+  }
 }
