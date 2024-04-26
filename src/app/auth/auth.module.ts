c8cd7d5a3291862227fea2397b52a77e40b5549a
Juan Vidal <juanmvg2003@gmail.com>
2024-04-25 22:07:54 -0500

    fue creada la pagina de login y el componente del formulario

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+
+// componentes
+import { LoginComponent } from './pages/login/login.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    AuthFormComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/auth/components/auth-form/auth-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input } from '@angular/core';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-form',
+  templateUrl: './auth-form.component.html',
+  styleUrls: ['./auth-form.component.css']
+})
+export class AuthFormComponent {
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private router:Router
+  ) {
+    this.builAuthForm();
+  }
+
+  // validar si se esta registrando si está ingresando
+  @Input() isInLoginView!:boolean;
+
+  // esquema del formulario
+  authForm!:FormGroup;
+  private builAuthForm(){
+    this.authForm = this.formBuilder.group({
+      user: ['',
+        [Validators.required,
+        Validators.maxLength(20)]
+      ],
+      
+      email: ['',
+        [Validators.email,
+          Validators.maxLength(50)
+        ]
+      ],
+
+      password: ['',
+      [Validators.required,
+      Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+    });
+  }
+
+  // validaciones de los campos del formulario}
+  get userField(){ return this.authForm.get('user') }
+  get passwordField(){ return this.authForm.get('password') }
+  get emailField(){ return this.authForm.get('email') }
+
+  // accion del formulario
+  handleSubmit(){
+    this.isInLoginView ? alert('Ingresaste a la aplicación') : alert('No ingresaste a la apklicación');
+  }
+
+  // cambiar auth
+  changeAuthState(){
+    if (this.isInLoginView) {
+      this.isInLoginView = false;
+      this.router.navigate(['/register']);
+    }else{
+      this.isInLoginView = true;
+      this.router.navigate(['/login']);
+    }
+
+  }
+
+}
